28e9f606db5005007e1324f68986673b18317577
JazRiv <110856832+JazRiv@users.noreply.github.com>
2023-04-12 15:44:20 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function tomar(){
+    save("MiFotoConFiltro.png")
+}
+
+function setup(){
+    canvas = createCanvas(600, 400);
+    canvas.center();
+    background("white");
+}
+
+function preload(){
+
+}
+
+function draw(){
+
+}
+
